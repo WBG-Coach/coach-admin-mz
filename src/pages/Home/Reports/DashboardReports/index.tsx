673c236a4ff2892b98e0cde8b5ca7bdb7d180276
Janderson Souza Matias <jandersonsouzamatias@Jandersons-MacBook-Pro.local>
2022-08-16 11:52:37 -0300

    fix: teacher and coache report

--- src/pages/Home/Reports/DashboardReports/index.tsx
+++ src/pages/Home/Reports/DashboardReports/index.tsx
@@ -21,14 +21,108 @@ export const DashboardReports = () => {
               lineHeight="24px"
               value="Teacher with the most sessions"
             />
+
+            <Container alignItems="center">
+              <Container
+                mr="16px"
+                width="40px"
+                height="40px"
+                overflow="hidden"
+                alignItems="center"
+                borderRadius="20px"
+                background="#F4F5F5"
+                justifyContent="center"
+              >
+                <Text fontSize="24px" color="#49504C" value={"D"} />
+              </Container>
+
+              <Container flexDirection="column" flex={1}>
+                <Text
+                  value="Dinho Barreto"
+                  fontWeight={500}
+                  fontSize="16px"
+                  lineHeight="24px"
+                />
+                <Text
+                  value="Opa! Escola de Design"
+                  fontWeight={400}
+                  color="#7D827F"
+                  fontSize="14px"
+                  lineHeight="20px"
+                />
+              </Container>
+
+              <Container>
+                <Text
+                  value="50"
+                  fontSize="24px"
+                  lineHeight="28px"
+                  fontWeight={600}
+                />
+              </Container>
+            </Container>
+            <Container
+              mt="8px"
+              height="4px"
+              borderRadius="2px"
+              width="100%"
+              background="#E4E7E5"
+            ></Container>
           </Card>
           <Card>
             <Text
-              mb="32px"
+              mb="16px"
               fontSize="18px"
               lineHeight="24px"
               value="Coach with the most sessions"
             />
+
+            <Container alignItems="center">
+              <Container
+                mr="16px"
+                width="40px"
+                height="40px"
+                overflow="hidden"
+                alignItems="center"
+                borderRadius="20px"
+                background="#F4F5F5"
+                justifyContent="center"
+              >
+                <Text fontSize="24px" color="#49504C" value={"D"} />
+              </Container>
+
+              <Container flexDirection="column" flex={1}>
+                <Text
+                  value="Dinho Barreto"
+                  fontWeight={500}
+                  fontSize="16px"
+                  lineHeight="24px"
+                />
+                <Text
+                  value="Opa! Escola de Design"
+                  fontWeight={400}
+                  color="#7D827F"
+                  fontSize="14px"
+                  lineHeight="20px"
+                />
+              </Container>
+
+              <Container>
+                <Text
+                  value="50"
+                  fontSize="24px"
+                  lineHeight="28px"
+                  fontWeight={600}
+                />
+              </Container>
+            </Container>
+            <Container
+              mt="8px"
+              height="4px"
+              borderRadius="2px"
+              width="100%"
+              background="#E4E7E5"
+            ></Container>
           </Card>
         </Container>
       </Container>
